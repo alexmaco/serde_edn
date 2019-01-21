c7a10d026220c00c77422a5e7f7abc6f9d7399ac
Alexandru Macovei <alexnmaco@gmail.com>
2019-01-21 09:24:03 +0200

    experimental wrapper type for Symbol

--- src/value/mod.rs
+++ src/value/mod.rs
@@ -20,18 +20,50 @@ pub enum Value {
 }
 
 #[derive(Debug, PartialEq, Clone)]
-pub enum Symbol {
-    Simple(String),
-    Namespaced { prefix: String, name: String },
+pub struct Symbol {
+    inner: String,
 }
 
 #[derive(Debug, PartialEq, Clone)]
 pub enum Tagged {
     Inst(std::time::Instant),
     UUID(u128),
-    User {
-        prefix: String,
-        name: String,
-        value: Box<Value>,
-    },
+    User(Symbol, Box<Value>),
+}
+
+// TODO: these are just test implementations
+impl Symbol {
+    pub fn full(&self) -> &str {
+        &self.inner
+    }
+
+    pub fn name(&self) -> &str {
+        if self.inner.contains('/') {
+            self.split('/').nth(1).unwrap()
+        } else {
+            &self.inner
+        }
+    }
+
+    pub fn namespace(&self) -> Option<&str> {
+        if self.inner.contains('/') {
+            self.split('/').nth(0)
+        } else {
+            None
+        }
+    }
+}
+
+impl Into<String> for Symbol {
+    fn into(self) -> String {
+        self.inner
+    }
+}
+
+impl std::ops::Deref for Symbol {
+    type Target = str;
+
+    fn deref(&self) -> &str {
+        &self.inner
+    }
 }
